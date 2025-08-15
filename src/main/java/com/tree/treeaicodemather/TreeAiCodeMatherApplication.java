523c0d286ca478fd4aae58ef3e973ee9a46d9afc
lefttooth <lefttooth@qq.com>
2025-08-15 16:34:06 +0800

    langchain

--- src/main/java/com/tree/treeaicodemather/TreeAiCodeMatherApplication.java
+++ src/main/java/com/tree/treeaicodemather/TreeAiCodeMatherApplication.java
@@ -4,11 +4,10 @@ import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStor
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
 @MapperScan("com.tree.treeaicodemather.mapper")
-@EnableAspectJAutoProxy(exposeProxy = true)
+
 @SpringBootApplication(exclude = {RedisEmbeddingStoreAutoConfiguration.class})
 
 
---
+++ src/test/java/com/tree/treeaicodemather/langgraph4j/ai/ImageCollectionServiceTest.java
@@ -0,0 +1,12 @@
+package com.tree.treeaicodemather.langgraph4j.ai;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageCollectionServiceTest {
+
+    @Test
+    void collectImages() {
+    }
+}
